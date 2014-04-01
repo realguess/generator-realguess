8a78b36592d32e9de238edefd0c2e7ed276b3b85
Chao Huang <chao@realguess.net>
2014-03-31 22:52:06 -0700

    Add a Bash script template

---
+++ app/templates/bash/_script.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# <%= appname %>
+# <%= new Array(appname.length + 1).join('=') %>
+#
+# > (c) <%= new Date().getFullYear() %> <%= pkg.author.name %> <<%= pkg.author.email %>>
