956ebd241be618399002b10aa13dea8b85ee4afe
Chao Huang <chao@realguess.net>
2014-07-06 10:33:54 -0700

    Add source code documentation format

--- gist/templates/gist.js
+++ gist/templates/gist.js
@@ -4,3 +4,34 @@
 // > (c) <%= new Date().getFullYear() %> Chao Huang <chao@realguess.net>
 
 'use strict';
+
+
+// Dependencies
+// ============
+//
+// Define module dependencies.
+var request = require('request');
+var _       = require('underscore');
+
+
+// Functions
+// =========
+//
+// Define internal functions.
+
+// My Func
+// -------
+function myFunc() {
+}
+
+
+// Exports
+// =======
+//
+// Define module exports.
+
+// Foo
+// ---
+exports.foo = function foo() {
+  return 'Foo';
+};
